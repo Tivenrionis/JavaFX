9cc72d11ce4f8df76b3f31ba2fbe1a59cce1b97a
Tiven <tivenstudio@gmail.com>
2020-06-01 13:19:44 +0200

    add updating the label by using UI Thread and Background Thread

--- src/Events/Controller.java
+++ src/Events/Controller.java
@@ -1,10 +1,15 @@
 package Events;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+// NIGDY NIE ZAKLADAJ, ZE USER POSTAPI TAK JAK ZAPLANOWALES
+// TRZEBA SPRAWDZAC KAZDA MOZLIWOSC
 public class Controller {
     @FXML
     private TextField nameField;
@@ -12,6 +17,10 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox outCheckbBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize() {
@@ -26,7 +35,38 @@ public class Controller {
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
         }
-        System.out.println("The following button was clicked " + e.getSource() + "\n");
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                //causes the freeze of the application
+                try {
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    System.out.println("Im going to sleep on the " + s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            System.out.println("Im updating the label at " + s);
+                            ourLabel.setText("We did something");
+                        }
+                    });
+                    //ourLabel.setText("We did something");
+
+                } catch (InterruptedException interruptedException) {
+                    //  interruptedException.printStackTrace();
+                }
+            }
+        };
+
+       new Thread(task).start();
+
+        if (outCheckbBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+        //System.out.println("The following button was clicked " + e.getSource() + "\n");
     }
 
     @FXML
@@ -36,5 +76,10 @@ public class Controller {
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
     }
+
+    public void handleChange() {
+        System.out.println("The checkbox is " + (outCheckbBox.isSelected() ? "checked" : "not checked"));
+    }
+
 
 }
