63be1b32d687fbe305614722f5055bfc6ccb722e
Tiven <tivenstudio@gmail.com>
2020-06-04 11:40:17 +0200

    add ToDoListApp package and implement simple view of the todo list -- implement control logic and handlers also

---
+++ src/ToDoListApp/Controller.java
@@ -0,0 +1,80 @@
+package ToDoListApp;
+
+import ToDoListApp.dataModel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<TodoItem> todoItems;
+    @FXML
+    private Label deadLineLabel;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea textArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+                LocalDate.of(2016, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+                LocalDate.of(2016, Month.MAY, 23));
+        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website rockups by Friday 22nd April",
+                LocalDate.of(2016, Month.APRIL, 22));
+        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+                LocalDate.of(2016, Month.MARCH, 23));
+        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wendsday",
+                LocalDate.of(2016, Month.APRIL, 20));
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            // old poprzedni wybrany item, new nowo wybrany item ... by default oba null
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+
+                if (newValue != null) {
+                    TodoItem item=todoListView.getSelectionModel().getSelectedItem();
+                    textArea.setText(item.getDetails());
+                }
+
+            }
+        });
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+
+    }
+
+    //odkad ustawilismy change listener to ta funkcja jest nam niepotrzebna bo change listener wystepuje w roli event handlera
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        textArea.setText(item.getDetails());
+        deadLineLabel.setText(item.getDeadLine().toString());
+        //System.out.println(item);
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadLine().toString());
+        //     textArea.setText(sb.toString());
+
+
+    }
+
+}
---
+++ src/ToDoListApp/Main.java
@@ -0,0 +1,24 @@
+package ToDoListApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//init start stop
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+       Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        primaryStage.setTitle("ToDo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
