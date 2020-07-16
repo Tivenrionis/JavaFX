88faedcb10eaf4de252214958877d2a8fd6d63f8
Tiven <tivenstudio@gmail.com>
2020-07-16 12:11:01 +0200

    Make progress bar, label and bind it with data from background thread

--- src/BackgroundThreads/Controller.java
+++ src/BackgroundThreads/Controller.java
@@ -4,31 +4,46 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 //Thread  in FX UI extends Task or Worker
 public class Controller {
     private Task<ObservableList<String>> task;
     @FXML
     private ListView listView;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label progressLabel;
 
 
     public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                ObservableList<String> employees = FXCollections.observableArrayList(
-                        "Michal Wes",
+
+                String[] names = {"Michal Wes",
                         "Julia Wes",
                         "Wes Michal",
-                        "Wes Julia"
-                );
+                        "Wes Julia", "Wes KID", "Wes ROFT", "Wes LOR", "LOR Julia"};
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for (int i = 0; i < 8; i++) {
+                    employees.add(names[i]);
+                    progressBar.setVisible(true);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 8);
+                    Thread.sleep(200);
+                }
 
                 return employees;
             }
         };
-
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
